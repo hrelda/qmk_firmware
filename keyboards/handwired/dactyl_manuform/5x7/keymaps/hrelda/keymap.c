ce80ecd08fdf004d9cc0b7b416fa4f8fa365c766
hrelda <cadorcadorcador@gmail.com>
2024-02-24 14:59:13 +1100

    fix some shift keys. try toggle layer shift and real shift but i dont think it works

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/hrelda/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/hrelda/keymap.c
@@ -22,6 +22,7 @@
 enum {
     TD_CAPS_WORD, //capword, caps lock, single press turns off capslock
     TD_RCTLALT, // ctrl, alt
+    TD_MODVORARK_SHIFT, //mo to _pdovark on press, normal shift on hold
 };
 
 void td_fn_capsw_toggle(tap_dance_state_t *state, void *user_data)
@@ -40,10 +41,29 @@ void td_fn_capsw_toggle(tap_dance_state_t *state, void *user_data)
     }
 }
 
+void td_fn_shift_layer_toggle_on(tap_dance_state_t *state, void *user_data)
+{
+    //uint8_t layer = biton32(layer_state);
+    switch(state->count){
+        case 1:
+            layer_on(_SHIFTED_PDVORAK);
+        break;
+        case 2:
+            register_code(MOD_LSFT);
+        break;
+    }
+}
+
+// void td_fn_shift_layer_toggle_off(tap_dance_state_t *state, void *user_data)
+// {
+//    layer_on(_PDVORAK);
+// }
+
 tap_dance_action_t tap_dance_actions[] = {
     [TD_CAPS_WORD] = ACTION_TAP_DANCE_FN(td_fn_capsw_toggle),
     //[TD_CAPS_WORD] = ACTION_TAP_DANCE_DOUBLE(QK_CAPS_WORD_TOGGLE,KC_CAPS),
     [TD_RCTLALT]   = ACTION_TAP_DANCE_DOUBLE(KC_RCTL,KC_RALT),
+    [TD_MODVORARK_SHIFT] = ACTION_TAP_DANCE_FN_ADVANCED(td_fn_shift_layer_toggle_on, NULL, NULL),
 };
 
 
@@ -70,45 +90,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_B,        KC_M,        KC_W,           KC_V,     KC_Z,        MO(_SHIFTED_PDVORAK),
                                                  KC_DOWN,        KC_UP,    KC_CIRCUMFLEX,  KC_PERCENT,
                   KC_ENTER,      KC_SPACE,
-                  KC_LGUI,       TD(TD_RCTLALT),
+                  KC_LGUI,       KC_RCTL,
                   KC_PGUP,       KC_PGDN
 
     ),
     [_SHIFTED_PDVORAK] = LAYOUT_5x7(
         // left hand
-        LSFT(KC_4),  KC_1,                KC_2,            KC_3,          KC_4,        KC_5,        KC_EQUAL,
-        LSFT(KC_TAB),      LSFT(KC_SEMICOLON),  LSFT(KC_COMMA),  LSFT(KC_DOT),  LSFT(KC_P),  LSFT(KC_Y),  TO(_QWERTY),
-        LSFT(KC_ESCAPE),   LSFT(KC_A),          LSFT(KC_O),      LSFT(KC_E),    LSFT(KC_U),  LSFT(KC_I),  TO(_FN),
-        KC_TRNS,     LSFT(KC_QUOTE),      LSFT(KC_Q),      LSFT(KC_J),    LSFT(KC_K),  LSFT(KC_X),
-        KC_TRNS,     KC_TRNS,             LSFT(KC_LEFT),         LSFT(KC_RIGHT),
-                                                                          KC_TRNS, KC_TRNS,
-                                                                          KC_TRNS, KC_TRNS,
-                                                                          LSFT(KC_END), LSFT(KC_HOME),
+        LSFT(KC_4),      KC_1,                KC_2,            KC_3,          KC_4,        KC_5,        KC_EQUAL,
+        LSFT(KC_TAB),    LSFT(KC_SEMICOLON),  LSFT(KC_COMMA),  LSFT(KC_DOT),  LSFT(KC_P),  LSFT(KC_Y),  TO(_QWERTY),
+        LSFT(KC_ESCAPE), LSFT(KC_A),          LSFT(KC_O),      LSFT(KC_E),    LSFT(KC_U),  LSFT(KC_I),  TO(_FN),
+        KC_TRNS,         LSFT(KC_QUOTE),      LSFT(KC_Q),      LSFT(KC_J),    LSFT(KC_K),  LSFT(KC_X),
+        KC_TRNS,         KC_TRNS,             LSFT(KC_LEFT),   LSFT(KC_RIGHT),
+                                                                          LSFT(KC_BACKSPACE), LSFT(KC_DELETE),
+                                                                          LSFT(KC_LCTL),      LSFT(KC_LALT),
+                                                                          LSFT(KC_END),       LSFT(KC_HOME),
         // right hand
-        LSFT(KC_8),          KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        LSFT(KC_2),
-        LSFT(KC_BACKSLASH),  LSFT(KC_F),  LSFT(KC_G),  LSFT(KC_C),  LSFT(KC_R),  LSFT(KC_L),  LSFT(KC_SLASH),
-        LSFT(KC_GRAVE),      LSFT(KC_D),  LSFT(KC_H),  LSFT(KC_T),  LSFT(KC_N),  LSFT(KC_S),  LSFT(KC_MINUS),
-                             LSFT(KC_B),  LSFT(KC_M),  LSFT(KC_W),  LSFT(KC_V),  LSFT(KC_Z),  KC_TRNS,
-                                                       LSFT(KC_DOWN), LSFT(KC_UP), KC_TRNS,   KC_TRNS,
-                  KC_TRNS, KC_TRNS,
-                  KC_TRNS, KC_TRNS,
-                  LSFT(KC_PGUP), LSFT(KC_PGDN)
+        LSFT(KC_8),          KC_6,        KC_7,        KC_8,          KC_9,        KC_0,        LSFT(KC_2),
+        LSFT(KC_BACKSLASH),  LSFT(KC_F),  LSFT(KC_G),  LSFT(KC_C),    LSFT(KC_R),  LSFT(KC_L),  LSFT(KC_SLASH),
+        LSFT(KC_GRAVE),      LSFT(KC_D),  LSFT(KC_H),  LSFT(KC_T),    LSFT(KC_N),  LSFT(KC_S),  LSFT(KC_MINUS),
+                             LSFT(KC_B),  LSFT(KC_M),  LSFT(KC_W),    LSFT(KC_V),  LSFT(KC_Z),  KC_TRNS,
+                                                       LSFT(KC_DOWN), LSFT(KC_UP), KC_TRNS,     KC_TRNS,
+                  LSFT(KC_ENTER),      LSFT(KC_SPACE),
+                  LSFT(KC_LGUI),       LSFT(KC_RCTL),
+                  LSFT(KC_PGUP),       LSFT(KC_PGDN)
     ),
     [_FN] = LAYOUT_5x7(
         // left hand
-        QK_BOOT,   KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,    KC_F6,
+        QK_BOOT,   KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       _______,
         KC_VOLU,   _______,     LCTL(KC_W),  _______,     _______,     LCTL(KC_Y),  TO(_PDVORAK),
-        KC_VOLD,   LCTL(KC_A),  LCTL(KC_S),  LCTL(KC_D),  LCTL(KC_F),  _______,  TO(_QWERTY),
+        KC_VOLD,   LCTL(KC_A),  LCTL(KC_S),  LCTL(KC_D),  LCTL(KC_F),  _______,     TO(_QWERTY),
         KC_MUTE,   LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C),  LCTL(KC_V),  TO(_LH_DEBUG),
         _______,   WIN_SS,      KC_CALC,     KC_MYCM,
                                     _______, _______,
                                     _______, _______,
                                     _______, _______,
         // right hand
-                          KC_F7,     KC_F8,           KC_F9,     KC_F10,    KC_F11,     KC_F12,       KC_NUM_LOCK,
-                          _______,   KC_KP_ASTERISK,  KC_KP_7,   KC_KP_8,   KC_KP_9,    KC_KP_MINUS,  KC_SCROLL_LOCK,
+               KC_NUM_LOCK,          KC_F6,           KC_F7,     KC_F8,     KC_F9,      KC_F10,       KC_F11,
+               KC_SCROLL_LOCK,       KC_KP_ASTERISK,  KC_KP_7,   KC_KP_8,   KC_KP_9,    KC_KP_MINUS,  KC_F12,
                TO(_QWERTY_TWOSPC),   KC_KP_SLASH,     KC_KP_4,   KC_KP_5,   KC_KP_6,    KC_KP_PLUS,   _______,
-                                     _______,         KC_KP_1,   KC_KP_2,   KC_KP_3,    KC_KP_ENTER,      KC_KP_ENTER,
+                                     _______,         KC_KP_1,   KC_KP_2,   KC_KP_3,    KC_KP_ENTER,  KC_KP_ENTER,
                                                       KC_KP_0,   KC_KP_DOT,  _______,   _______,
              KC_DEL, KC_ENTER,
              _______, _______,
@@ -143,8 +163,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESCAPE,     KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   TO(_FN),
         MOD_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
         TT(_FN),    TD(TD_CAPS_WORD), KC_LEFT,   KC_RIGHT,
-                                                         KC_BACKSPACE,         KC_DELETE,
-                                                        KC_LCTL,  KC_LALT,
+                                                         KC_BACKSPACE,      KC_DELETE,
+                                                         KC_LCTL,           KC_LALT,
                                                          KC_END,            KC_HOME,
         // right hand
                           KC_CALC, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_EQUAL,
@@ -160,20 +180,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MINUS,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_GRAVE,
         KC_TAB,      KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   TO(_PDVORAK),
         KC_ESCAPE,     KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   TO(_FN),
-        OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
+        MOD_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
         TT(_FN),    TD(TD_CAPS_WORD), KC_LEFT,   KC_RIGHT,
                                                          KC_SPACE,         KC_BACKSPACE,
-                                                         OSM(MOD_LALT),  OSM(MOD_LCTL),
-                                                         KC_END,            KC_HOME,
+                                                         KC_LCTL,          KC_LALT,
+                                                         KC_END,           KC_HOME,
         // right hand
                           KC_CALC, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_EQUAL,
                           KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
                           _______, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),
+                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  MOD_RSFT,
                                             KC_DOWN, KC_UP,    KC_LBRC,  KC_RBRC,
                   KC_ENTER,      KC_SPACE,
                   KC_PGUP,       KC_PGDN,
-                  OSM(MOD_RCTL), OSM(KC_RGUI)
+                  MOD_RCTL, KC_RGUI
     ),
 };
 
